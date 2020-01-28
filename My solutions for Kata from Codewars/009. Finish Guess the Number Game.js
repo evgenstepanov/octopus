47513b168aa5e757f60eaff5a1912b7f0cc4290e
evgenstepanov <evgenstepanov@users.noreply.github.com>
2020-01-28 19:11:09 +0300

    new

---
+++ My solutions for Kata from Codewars/009. Finish Guess the Number Game.js
@@ -0,0 +1,21 @@
+/*
+Imagine you are creating a game where the user has to guess the correct number. 
+But there is a limit of how many guesses the user can do.
+
+If the user tries to guess WRONG more than the limit the function should throw an error
+If the user guess wrong it should lose a life and return false (if you guess correctly you shouldn't remove a life)
+If the user guess right it should return true
+*/
+
+class Guesser {
+  constructor(number, lives) {
+    this.number = number;
+    this.lives = lives;
+  }
+
+  guess(n) {
+    if (this.lives < 1) throw 'Dead';
+    if (this.number !== n) --this.lives;
+    return this.number === n;
+  }
+}
