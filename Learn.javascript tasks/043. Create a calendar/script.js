262434b24558671c91037574acefca122683cef6
evgenstepanov <evgenos@list.ru>
2020-03-25 16:26:20 +0300

    made last row with empty td

--- Learn.javascript tasks/043. Create a calendar/script.js
+++ Learn.javascript tasks/043. Create a calendar/script.js
@@ -1,23 +1,34 @@
 function createCalendar(elem, year, month) {
+  let counter = 1;
   let date = new Date(year, month - 1);
-  let day = date.getDay() === 0 ? 7 : date.getDay();
+  let dayOfWeek = date.getDay() === 0 ? 7 : date.getDay();
   let table = document.createElement('table');
   table.innerHTML =
-    '<tr><th>Mon</th><th>Tu</th><th>We</th><th>Th</th><th>Fr</th><th>Sa</th><th>Su</th></tr>';
+    '<tr><th>Mo</th><th>Tu</th><th>We</th><th>Th</th><th>Fr</th><th>Sa</th><th>Su</th></tr>';
   elem.append(table);
 
-  let counter = 1;
-  for (let i = 0; i < 6; ++i) {
+  outer: for (let i = 1; i <= 7; ++i) {
     let row = table.insertRow();
     console.log(row);
 
     for (let j = 1; j <= 7; ++j) {
       let td = document.createElement('td');
-      td.innerText = counter++;
-      row.append(td);
-      if (row.cells.length === 7) break;
+      if (j >= dayOfWeek || counter > 1) {
+        td.innerText = counter;
+        counter++;
+        row.append(td);
+      } else {
+        td.innerText = '';
+        row.append(td);
+      }
+      if (new Date(year, month - 1, counter - 1).getMonth() !== month - 1) {
+        td.innerText = '';
+        row.append(td);
+      }
     }
+    if (new Date(year, month - 1, counter - 1).getMonth() !== month - 1)
+      break outer;
   }
 }
 
-createCalendar(calendar, 2012, 9);
+createCalendar(calendar, 2020, 1);
