f4483165490dfbe51d50b1b8c872bee7936f50ee
evgenstepanov <evgenos@list.ru>
2020-03-26 23:02:49 +0300

    added ending of table

--- Learn.javascript tasks/043. Create a calendar/script.js
+++ Learn.javascript tasks/043. Create a calendar/script.js
@@ -1,34 +1,41 @@
+year.onchange = month.onchange = function() {
+  if (year.value === '' || month.value === '') return;
+  let table = document.getElementById('table');
+  if (table) table.remove();
+  createCalendar(calendar, year.value, month.value);
+};
+
 function createCalendar(elem, year, month) {
-  let counter = 1;
-  let date = new Date(year, month - 1);
+  let monthJs = month - 1;
+  let date = new Date(year, monthJs);
   let dayOfWeek = date.getDay() === 0 ? 7 : date.getDay();
   let table = document.createElement('table');
+  table.id = 'table';
   table.innerHTML =
     '<tr><th>Mo</th><th>Tu</th><th>We</th><th>Th</th><th>Fr</th><th>Sa</th><th>Su</th></tr>';
   elem.append(table);
 
-  outer: for (let i = 1; i <= 7; ++i) {
+  let counter = 1;
+  while (true) {
     let row = table.insertRow();
-    console.log(row);
-
     for (let j = 1; j <= 7; ++j) {
       let td = document.createElement('td');
-      if (j >= dayOfWeek || counter > 1) {
+      if ((j >= dayOfWeek || counter > 1) && date.getMonth() === monthJs) {
         td.innerText = counter;
-        counter++;
-        row.append(td);
+        date = new Date(year, monthJs, counter);
+        if (date.getMonth() !== monthJs) {
+          td.innerText = '';
+          row.append(td);
+        } else {
+          counter++;
+          row.append(td);
+        }
       } else {
         td.innerText = '';
         row.append(td);
       }
-      if (new Date(year, month - 1, counter - 1).getMonth() !== month - 1) {
-        td.innerText = '';
-        row.append(td);
-      }
     }
-    if (new Date(year, month - 1, counter - 1).getMonth() !== month - 1)
-      break outer;
+    date = new Date(year, monthJs, counter);
+    if (date.getMonth() !== monthJs) break;
   }
 }
-
-createCalendar(calendar, 2020, 1);
