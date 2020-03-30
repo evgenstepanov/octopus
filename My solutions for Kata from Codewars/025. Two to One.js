f5c49ede6efa65bb0954a341958e196443a7ef04
evgenstepanov <evgenos@list.ru>
2020-03-30 18:48:17 +0300

    two to one

---
+++ My solutions for Kata from Codewars/025. Two to One.js
@@ -0,0 +1,10 @@
+// Take 2 strings s1 and s2 including only letters from ato z.
+// Return a new sorted string, the longest possible, containing distinct letters,
+
+// Each taken only once - coming from s1 or s2.
+
+function longest(s1, s2) {
+  return Array.from(new Set(s1 + s2))
+    .sort()
+    .join('');
+}
